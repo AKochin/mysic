d449dd5568d80e5ce179a727c7d7530afbcc4b1e
Andrey Kochin <Akochin@example.com>
2018-09-11 17:34:47 +0300

    DB preparations

--- webby/music/models.py
+++ webby/music/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    atitle = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    alogo = models.FileField()
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    stitle = models.CharField(max_length=250)
+    type = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.song_title

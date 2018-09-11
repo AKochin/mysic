e40e67201daa3182d9630f08b71936bb85f8a34b
Andrey Kochin <Akochin@example.com>
2018-09-11 20:17:49 +0300

    Lesson 21

--- webby/music/urls.py
+++ webby/music/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),

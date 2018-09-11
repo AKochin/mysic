e541bbc5051a06af31f00e1c5503ff8fda5315d8
Andrey Kochin <Akochin@example.com>
2018-09-11 18:08:03 +0300

    COrrected Albums logo field to ext source link

--- webby/music/models.py
+++ webby/music/models.py
@@ -6,7 +6,7 @@ class Album(models.Model):
     artist = models.CharField(max_length=250)
     atitle = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    alogo = models.FileField()
+    alogo = models.CharField(max_length=250)
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
